bc7f72bd097b1054c98e2d66e5f61eaba6c17563
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2024-05-24 17:25:50 +0200

    Win32 Backend Update
    -Moved all methods to the Backends::Win32 namespace
    -Added hooks for GetCursor and SetCursor
    -Allow WM_QUIT to pass to the original WndProc

--- SharpGUINative/backends/win32_backend.cpp
+++ SharpGUINative/backends/win32_backend.cpp
@@ -6,6 +6,8 @@
 
 #include "minhook/include/MinHook.h"
 
+#include <map>
+
 typedef bool (WINAPI* GetCursorPosDef)(LPPOINT lpPoint);
 typedef bool (WINAPI* SetCursorPosDef)(int X, int Y);
 
@@ -14,13 +16,8 @@ typedef int (WINAPI* ShowCursorDef)(bool bShow);
 typedef BOOL(WINAPI* ClipCursorDef)(const RECT* lpRect);
 typedef BOOL(WINAPI* GetClipCursorDef)(LPRECT lpRect);
 
-GetCursorPosDef oGetCursorPos;
-SetCursorPosDef oSetCursorPos;
-
-ShowCursorDef oShowCursor;
-
-ClipCursorDef oClipCursor;
-GetClipCursorDef oGetClipCursor;
+typedef HCURSOR(WINAPI* GetCursorDef)();
+typedef HCURSOR(WINAPI* SetCursorDef)(HCURSOR hCursor);
 
 namespace Backends::Win32
 {
@@ -30,13 +27,52 @@ namespace Backends::Win32
 
 	WNDPROC oWndProc = nullptr;
 
-	bool cursorVisible = false;
+	bool initCursor = false;
 	bool resetCursorState = false;
+
+	bool cursorVisible = false;
 	POINT cursorPos{};
 	RECT cursorClip{};
+	HCURSOR cursor;
+
+	// Utilities
+	bool IsCursorVisible();
+	int HideMouseCursor();
+	int ShowMouseCursor();
+	bool IsImguiCursor(HCURSOR hCursor);
+
+	// Win API Hooks originals
+	GetCursorPosDef oGetCursorPos;
+	SetCursorPosDef oSetCursorPos;
+
+	ShowCursorDef oShowCursor;
+
+	ClipCursorDef oClipCursor;
+	GetClipCursorDef oGetClipCursor;
+
+	GetCursorDef oGetCursor;
+	SetCursorDef oSetCursor;
+
+	// Win API Hooks
+	bool WINAPI hkGetCursorPos(LPPOINT lpPoint);
+	bool WINAPI hkSetCursorPos(int X, int Y);
+
+	int WINAPI hkShowCursor(bool bShow);
+
+	BOOL WINAPI hkClipCursor(const RECT* lpRect);
+	BOOL WINAPI hkGetClipCursor(LPRECT lpRect);
+
+	HCURSOR WINAPI hkGetCursor();
+	HCURSOR WINAPI hkSetCursor(HCURSOR hCursor);
+
+	LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	// Hooking helpers
+	void CreateAndEnableHook(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal);
+	void DisableAndRemoveHook(LPVOID pTarget);
 }
 
-bool IsCursorVisible()
+bool Backends::Win32::IsCursorVisible()
 {
 	CURSORINFO cursorInfo;
 	cursorInfo.cbSize = sizeof(cursorInfo);
@@ -45,21 +81,51 @@ bool IsCursorVisible()
 	return cursorInfo.flags == CURSOR_SHOWING;
 }
 
-int HideMouseCursor()
+int Backends::Win32::HideMouseCursor()
 {
 	int count;
 	while ((count = oShowCursor(false)) >= 0);
 	return count;
 }
 
-int ShowMouseCursor()
+int Backends::Win32::ShowMouseCursor()
 {
 	int count;
 	while ((count = oShowCursor(true)) <= 0);
 	return count;
 }
 
-bool WINAPI hkGetCursorPos(LPPOINT lpPoint)
+bool Backends::Win32::IsImguiCursor(HCURSOR hCursor)
+{
+	static std::map<ImGuiMouseCursor, HCURSOR> cursorMap;
+
+	if (cursorMap.size() == 0)
+	{
+#define ADD_CURSOR(imguiCursor, cursor) cursorMap[imguiCursor] = LoadCursor(nullptr, cursor);
+
+		cursorMap[ImGuiMouseCursor_None] = nullptr;
+		ADD_CURSOR(ImGuiMouseCursor_Arrow, IDC_ARROW);
+		ADD_CURSOR(ImGuiMouseCursor_TextInput, IDC_IBEAM);
+		ADD_CURSOR(ImGuiMouseCursor_ResizeAll, IDC_SIZEALL);
+		ADD_CURSOR(ImGuiMouseCursor_ResizeEW, IDC_SIZEWE);
+		ADD_CURSOR(ImGuiMouseCursor_ResizeNS, IDC_SIZENS);
+		ADD_CURSOR(ImGuiMouseCursor_ResizeNESW, IDC_SIZENESW);
+		ADD_CURSOR(ImGuiMouseCursor_ResizeNWSE, IDC_SIZENWSE);
+		ADD_CURSOR(ImGuiMouseCursor_Hand, IDC_HAND);
+		ADD_CURSOR(ImGuiMouseCursor_NotAllowed, IDC_NO);
+
+#undef ADD_CURSOR
+	}
+
+	ImGuiMouseCursor imguiCursor = igGetMouseCursor();
+
+	if (!cursorMap.contains(imguiCursor))
+		return false;
+
+	return cursorMap[imguiCursor] == hCursor;
+}
+
+bool WINAPI Backends::Win32::hkGetCursorPos(LPPOINT lpPoint)
 {
 	if (Backends::Win32::handleInput)
 	{
@@ -74,7 +140,7 @@ bool WINAPI hkGetCursorPos(LPPOINT lpPoint)
 	return oGetCursorPos(lpPoint);
 }
 
-bool WINAPI hkSetCursorPos(int X, int Y)
+bool WINAPI Backends::Win32::hkSetCursorPos(int X, int Y)
 {
 	if (Backends::Win32::handleInput)
 	{
@@ -87,7 +153,7 @@ bool WINAPI hkSetCursorPos(int X, int Y)
 	return oSetCursorPos(X, Y);
 }
 
-int WINAPI hkShowCursor(bool bShow)
+int WINAPI Backends::Win32::hkShowCursor(bool bShow)
 {
 	Backends::Win32::cursorVisible = bShow;
 
@@ -101,7 +167,7 @@ int WINAPI hkShowCursor(bool bShow)
 	return count;
 }
 
-BOOL WINAPI hkClipCursor(const RECT* lpRect)
+BOOL WINAPI Backends::Win32::hkClipCursor(const RECT* lpRect)
 {
 	if (lpRect != nullptr)
 	{
@@ -119,7 +185,7 @@ BOOL WINAPI hkClipCursor(const RECT* lpRect)
 	return oClipCursor(lpRect);
 }
 
-BOOL WINAPI hkGetClipCursor(LPRECT lpRect)
+BOOL WINAPI Backends::Win32::hkGetClipCursor(LPRECT lpRect)
 {
 	if (Backends::Win32::handleInput)
 	{
@@ -130,10 +196,39 @@ BOOL WINAPI hkGetClipCursor(LPRECT lpRect)
 	return oGetClipCursor(lpRect);
 }
 
-LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+HCURSOR WINAPI Backends::Win32::hkGetCursor()
+{
+	if (Backends::Win32::handleInput)
+		return Backends::Win32::cursor;
+
+	return oGetCursor();
+}
+
+HCURSOR WINAPI Backends::Win32::hkSetCursor(HCURSOR hCursor)
+{
+	HCURSOR oldCursor = Backends::Win32::cursor;
+	Backends::Win32::cursor = hCursor;
+
+	if (Backends::Win32::handleInput && !IsImguiCursor(hCursor))
+	{
+		return oldCursor;
+	}
+
+	return oSetCursor(hCursor);
+}
+
+LRESULT __stdcall Backends::Win32::WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	if (Backends::Win32::handleInput)
 	{
+		// Init cursor
+		if (Backends::Win32::initCursor)
+		{
+			oSetCursor(LoadCursor(nullptr, IDC_ARROW));
+
+			Backends::Win32::initCursor = false;
+		}
+
 		if (!IsCursorVisible())
 		{
 			ShowMouseCursor();
@@ -177,6 +272,7 @@ LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 		case WM_NCHITTEST:
 		case WM_NCMOUSEMOVE:
 		case WM_SETCURSOR:
+		case WM_QUIT:
 			callOriginal = true;
 			break;
 
@@ -236,9 +332,13 @@ void Backends::Win32Backend::SetHandleInput(bool handleInput)
 
 	if (handleInput)
 	{
-		Backends::Win32::cursorVisible = IsCursorVisible();
-		oGetCursorPos(&Backends::Win32::cursorPos);
-		oGetClipCursor(&Backends::Win32::cursorClip);
+		Backends::Win32::cursorVisible = Backends::Win32::IsCursorVisible();
+		Backends::Win32::oGetCursorPos(&Backends::Win32::cursorPos);
+		Backends::Win32::oGetClipCursor(&Backends::Win32::cursorClip);
+
+		Backends::Win32::cursor = Backends::Win32::oGetCursor();
+
+		Backends::Win32::initCursor = true;
 	}
 	else
 	{
@@ -246,13 +346,13 @@ void Backends::Win32Backend::SetHandleInput(bool handleInput)
 	}
 }
 
-void CreateAndEnableHook(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal)
+void Backends::Win32::CreateAndEnableHook(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal)
 {
 	MH_CreateHook(pTarget, pDetour, ppOriginal);
 	MH_EnableHook(pTarget);
 }
 
-void DisableAndRemoveHook(LPVOID pTarget)
+void Backends::Win32::DisableAndRemoveHook(LPVOID pTarget)
 {
 	MH_DisableHook(pTarget);
 	MH_RemoveHook(pTarget);
@@ -278,6 +378,9 @@ void Backends::Win32::Initialize(HWND window)
 
 	CreateAndEnableHook(&ClipCursor, &hkClipCursor, (void**)&oClipCursor);
 	CreateAndEnableHook(&GetClipCursor, &hkGetClipCursor, (void**)&oGetClipCursor);
+
+	CreateAndEnableHook(&GetCursor, &hkGetCursor, (void**)&oGetCursor);
+	CreateAndEnableHook(&SetCursor, &hkSetCursor, (void**)&oSetCursor);
 
 	initialized = true;
 }
@@ -306,6 +409,9 @@ void Backends::Win32::Shutdown()
 
 	DisableAndRemoveHook(&ClipCursor);
 	DisableAndRemoveHook(&GetClipCursor);
+
+	DisableAndRemoveHook(&GetCursor);
+	DisableAndRemoveHook(&SetCursor);
 
 	Backends::Win32::window = NULL;
 	initialized = false;
